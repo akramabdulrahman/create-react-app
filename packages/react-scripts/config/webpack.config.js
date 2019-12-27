7f701f7303c8d1ad53a5aa89717ac0f911c88cc6
akramabdelrahman <a.akram@fixtag.com.sa>
2019-12-27 17:28:42 +0200

    add the postcss plugin where it belongs

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -118,6 +118,7 @@ module.exports = function(webpackEnv) {
               },
               stage: 3,
             }),
+            require('postcss-rtl')(),
             // Adds PostCSS Normalize as the reset css with default options,
             // so that it honors browserslist config in package.json
             // which in turn let's users customize the target behavior as per their needs.
